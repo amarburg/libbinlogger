0f681c5cc386b159d20f8c89ebd2c99760ef34ee
Aaron Marburg <amarburg@apl.washington.edu>
2016-11-08 11:23:09 -0800

    Working on getting conan configuration working.

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,13 @@
 from conans import ConanFile, CMake
 
-class LibActiveObjectConan(ConanFile):
+class LibLoggerConan(ConanFile):
   name = "liblogger"
   version = "0.1"
   settings = "os", "compiler", "build_type", "arch"
   generators = "cmake"
+  exports = "*"
+  options = {"opencv_dir": "ANY"}
+  default_options = "opencv_dir=''"
   requires = "TCLAP/master@jmmut/testing", \
               "snappy/1.1.3@hoxnox/testing", \
               "zlib/1.2.8@lasote/stable", \
@@ -22,8 +25,24 @@ class LibActiveObjectConan(ConanFile):
 
   def build(self):
     cmake = CMake(self.settings)
+    if self.options.opencv_dir:
+      cmake_opts = "-DOpenCV_DIR=%s" % (self.options.opencv_dir)
+
     flag_build_tests = "-DBUILD_UNIT_TESTS=1" if self.scope.dev and self.scope.build_tests else ""
-    self.run('cmake "%s" %s %s' % (self.conanfile_directory, cmake.command_line, flag_build_tests))
+    self.run('cmake "%s" %s %s %s' % (self.conanfile_directory, cmake.command_line, cmake_opts, flag_build_tests))
     self.run('cmake --build . %s' % cmake.build_config)
     if self.scope.dev and self.scope.build_tests:
       self.run('make unit_test')
+
+  def package(self):
+    self.copy("*.h", dst="")
+    #if self.options.shared:
+    if self.settings.os == "Macos":
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+    else:
+        self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
+    #else:
+    #    self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
+
+  def package_info(self):
+      self.cpp_info.libs = ["logger"]
